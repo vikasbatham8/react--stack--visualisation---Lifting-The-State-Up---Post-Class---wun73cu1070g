accb471c36816832aab1119e04c282dbf7291758
Viikas Batham <112746619+vikasbatham8@users.noreply.github.com>
2023-02-27 21:57:57 +0530

    Update element.js

--- src/components/element.js
+++ src/components/element.js
@@ -1,6 +1,6 @@
-import React from 'react';
-function Element() {
-  return <div className="element"></div>;
+import React from "react";
+function Element(props) {
+  return <div className="element">{props.val}</div>;
 }
 
 export default Element;
